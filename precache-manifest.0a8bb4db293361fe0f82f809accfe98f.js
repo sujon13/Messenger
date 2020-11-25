491f80d0969e04fee76102fffa2381d56205584d
sujon13 <arifurrahmansujon27@gmail.com>
2020-11-25 22:52:01 +0600

    Updates

---
+++ precache-manifest.0a8bb4db293361fe0f82f809accfe98f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "47c624fe01170ba6f3ffe3ed0b7722b4",
+    "url": "/Messenger/index.html"
+  },
+  {
+    "revision": "94c8d616d049366a654c",
+    "url": "/Messenger/static/js/2.5884fcc9.chunk.js"
+  },
+  {
+    "revision": "92e341951306a4e77250eae9ace760e5",
+    "url": "/Messenger/static/js/2.5884fcc9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aad5358fb49c278b1188",
+    "url": "/Messenger/static/js/main.57bdf0a6.chunk.js"
+  },
+  {
+    "revision": "7c186327ac7d2de13345",
+    "url": "/Messenger/static/js/runtime-main.a2d83765.js"
+  }
+]);
